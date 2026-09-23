71f3da7bf3cf008b11383e1e7cf4e3f26ef7a66f
Ming Han Chia <cmh9090@hotmail.com>
2026-09-23 23:35:52 +0800

    Add EC2 module structure for exercise 4 (pending AWS EC2 access)

---
+++ 04-remote-state/main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "random_id" "state_suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "iac-learning-tfstate-${random_id.state_suffix.hex}"
+
+  tags = {
+    Name    = "terraform-state"
+    Purpose = "terraform-learning"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "iac-learning-tf-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name    = "terraform-lock"
+    Purpose = "terraform-learning"
+  }
+}
